0374c2080b16bdfa227f4d6f0b39499051f4af59
Daniel Tran <danieltran770@gmail.com>
2021-08-11 22:35:02 -0400

    added lazy loading for non intro sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+// import Skills from './components/Skills';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
 import './styles/app.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'aos/dist/aos.css';
+
+const Skills = lazy(() => import('./components/Skills'));
+const Projects = lazy(() => import('./components/Projects'));
+const Contact = lazy(() => import('./components/Contact'));
 
 const App = () => {
 	return (
 		<div className='app'>
 			<Navbar />
 			<Intro />
-			<Skills />
-			<Projects />
-			<Contact />
+			<Suspense fallback={<div>Loading...</div>}>
+				<Skills />
+				<Projects />
+				<Contact />
+			</Suspense>
 		</div>
 	);
 };
